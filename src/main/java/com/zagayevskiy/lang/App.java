39ffc6db357c9bb24e7b8bb61c7e6356834b61b0
Denis Zagayevskiy <zagayevskiy@gmail.com>
2016-05-22 23:29:41 +0300

    Initial commit

---
+++ src/main/java/com/zagayevskiy/lang/App.java
@@ -0,0 +1,8 @@
+package com.zagayevskiy.lang;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
---
+++ src/test/java/com/zagayevskiy/lang/AppTest.java
@@ -0,0 +1,14 @@
+package com.zagayevskiy.lang;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AppTest {
+
+    @Test
+    public void example() {
+        assertThat("hello", is("world"));
+    }
+}
